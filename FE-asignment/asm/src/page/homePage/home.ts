059353e33baddf2e07ed24a6db552612864d0792
luannc2000 <luanncph13070@fpt.edu.vn>
2022-08-02 15:53:24 +0700

    #[5] Create HomePage

--- FE-asignment/asm/src/page/homePage/home.ts
+++ FE-asignment/asm/src/page/homePage/home.ts
@@ -1,44 +1,29 @@
-import headerPage from "../compument/header"
+import { getProducts } from "../../api/product"
+import Product from "../../model/product"
+import footer from "../compument/footer"
+import header from "../compument/header"
 import menu from "../compument/menu"
 
 const homePage = {
-	render: () => {
-		return (
-            /*html*/`
-            ${headerPage.render()}
+	render: async () => {
+        const res = await getProducts()
+        const data: Product[] = res.data
+        console.log(data.map((p) => (p.id)));
+		return /*html*/`
+            ${header.render()}
             ${menu.render()}
+            <h1 class="ml-20 mt-10 text-lg ">ĐIỆN THOẠI HOT NHẤT</h1> 
         <div class="product ">
-            <h1 class="ml-20 mt-10 text-lg ">ĐIỆN THOẠI NỔI BẬT NHẤT</h1>
-            <div class="grid grid-cols-5 gap-6 m-10">
-            <div>
-                <img src="./public/image/iPhone 11 64GB I Chính hãng VNA.jpg"  class="mb-2" alt="">
-                <p class="mb-2">iPhone 11 64GB I Chính hãng VNA</p>
-                <label class="text-red-700 pr-4">10.790.000 ₫</label><label class="text-gray-400">18.000.000 ₫</label>
-                <p class="bg-gray-100 p-3 mt-2">[HOT] Thu cũ lên đời giá cao - Thủ tục nhanh - Trợ giá lên tới 1.000.000đ</p>
-            </div>
-            <div>
-                <img src="./public/image/iPhone 11 64GB I Chính hãng VNA.jpg"  class="mb-2" alt="">
-                <p class="mb-2">iPhone 11 64GB I Chính hãng VNA</p>
-                <label class="text-red-700 pr-4">10.790.000 ₫</label><label class="text-gray-400">18.000.000 ₫</label>
-                <p class="bg-gray-100 p-3 mt-2">[HOT] Thu cũ lên đời giá cao - Thủ tục nhanh - Trợ giá lên tới 1.000.000đ</p>
-            </div>
-            <div>
-                <img src="./public/image/iPhone 11 64GB I Chính hãng VNA.jpg"  class="mb-2" alt="">
-                <p class="mb-2">iPhone 11 64GB I Chính hãng VNA</p>
-                <label class="text-red-700 pr-4">10.790.000 ₫</label><label class="text-gray-400">18.000.000 ₫</label>
-                <p class="bg-gray-100 p-3 mt-2">[HOT] Thu cũ lên đời giá cao - Thủ tục nhanh - Trợ giá lên tới 1.000.000đ</p>
-            </div>
-            <div>
-                <img src="./public/image/iPhone 11 64GB I Chính hãng VNA.jpg"  class="mb-2" alt="">
-                <p class="mb-2">iPhone 11 64GB I Chính hãng VNA</p>
-                <label class="text-red-700 pr-4">10.790.000 ₫</label><label class="text-gray-400">18.000.000 ₫</label>
-                <p class="bg-gray-100 p-3 mt-2">[HOT] Thu cũ lên đời giá cao - Thủ tục nhanh - Trợ giá lên tới 1.000.000đ</p>
-            </div>
-            <div>
-                <img src="./public/image/iPhone 11 64GB I Chính hãng VNA.jpg"  class="mb-2" alt="">
-                <p class="mb-2">iPhone 11 64GB I Chính hãng VNA</p>
-                <label class="text-red-700 pr-4">10.790.000 ₫</label><label class="text-gray-400">18.000.000 ₫</label>
-                <p class="bg-gray-100 p-3 mt-2">[HOT] Thu cũ lên đời giá cao - Thủ tục nhanh - Trợ giá lên tới 1.000.000đ</p>
+            
+            <div class=" grid grid-cols-5 gap-6 m-10">
+            ${data.map((p, index) => /*html*/`
+                <div>
+                    <a href="/${p.id}"><img src="${p.image}"  class="mb-2" alt=""></a>
+                    <a href="/${p.id}"><p class="mb-2">${p.name}</p></a>
+                    <label class="text-red-700 pr-4">${p.saleOffPrice}</label>
+                    <label class="text-gray-400">${p.originalPrice}</label>
+                    <p class="bg-gray-100 p-3 mt-2">${p.shortDescription}</p>
+                </div>`).join('')}
             </div>
         </div>
         <div class="phukien mx-[100px]">
@@ -89,50 +74,11 @@ const homePage = {
             </div>
         </div>
         </div>
-        <div class="linhkien mx-[100px] mb-[150px]">
-        <div class="grid grid-cols-2 mb-5 ">
-            <h1 class="ml-20 mt-10 text-lg">LINH KIỆN MÁY TÍNH</h1>
-            <a href="#" class="ml-20 mt-10 ">Xem tất cả</a>
-        </div>
-        <div class="grid grid-cols-9 gap-4 mx-20">
-            <div class="bg-[#FFB8B8] rounded-lg">
-            <p class="text-white">Phụ kiện Apple</p>
-            <img src="./public/image/linhkien.jpg" alt="">
-            </div>
-            <div class="bg-[#FF94EB] rounded-lg">
-            <p class="text-white">Phụ kiện Apple</p>
-            <img src="./public/image/linhkien.jpg" alt="">
-            </div>
-            <div class="bg-[#96FDB5] rounded-lg">
-            <p class="text-white">Phụ kiện Apple</p>
-            <img src="./public/image/linhkien.jpg" alt="">
-            </div>
-            <div class="bg-[#FF94EB] rounded-lg">
-            <p class="text-white">Phụ kiện Apple</p>
-            <img src="./public/image/linhkien.jpg" alt="">
-            </div>
-            <div class="bg-[#4D91FF] rounded-lg">
-            <p class="text-white">Phụ kiện Apple</p>
-            <img src="./public/image/linhkien.jpg" alt="">
-            </div>
-            <div class="bg-[#F5D63D] rounded-lg">
-            <p class="text-white">Phụ kiện Apple</p>
-            <img src="./public/image/linhkien.jpg" alt="">
-            </div>
-            <div class="bg-[#FDA363] rounded-lg">
-            <p class="text-white">Phụ kiện Apple</p>
-            <img src="./public/image/linhkien.jpg" alt="">
-            </div>
-            <div class="bg-[#D6D6D6] rounded-lg">
-            <p class="text-white">Phụ kiện Apple</p>
-            <img src="./public/image/linhkien.jpg" alt="">
-            </div>  
-        </div>
-        </div>
   </div>
-            `
-		)
-	}
+  <br>
+  <br>
+  ${footer.render()}
+        `}
 }
 
 export default homePage
