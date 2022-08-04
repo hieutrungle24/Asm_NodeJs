d1aec3c0e7984d0288708e3f34686e786101a723
quanlmph18475 <quanlmph18475@fpt.edu.vn>
2022-08-04 10:03:57 +0700

    #25 Fix the models product

--- BE-asignment/src/models/product.js
+++ BE-asignment/src/models/product.js
@@ -1,14 +1,24 @@
 import mongoose from "mongoose";
 const ProductSchema = mongoose.Schema ({
+    image_path: {
+        type: String,
+    },
     name: {
         type: String,
+    },
+    saleOffPrice: {
+        type: Number,
     },
     originalPrice: {
         type: Number,
     },
-    description: {
+    shortDescription: {
         type: String,
     },
+    category: {
+        type: mongoose.ObjectId,
+        ref: 'Category'
+    }
 });
  export default mongoose.model('Product',ProductSchema);
 
