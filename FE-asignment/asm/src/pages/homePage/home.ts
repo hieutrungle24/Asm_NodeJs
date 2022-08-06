5d665c3df679008ccff9aacd9a5e839af02c5d9b
hieutran <hieutdph13190@fpt.edu.vn>
2022-08-06 13:45:04 +0700

    update-show-category

--- FE-asignment/asm/src/pages/homePage/home.ts
+++ FE-asignment/asm/src/pages/homePage/home.ts
@@ -4,26 +4,30 @@ import footer from "../../compoment/footer"
 import header from "../../compoment/header"
 import menu from "../../compoment/menu"
 import banner from "../../compoment/banner"
-
+import Category from "../../model/category"
+import { getCategoryid } from "../../api/category"
 const homePage = {
     render: async () => {
         const res = await getProducts()
         const data: Product[] = res.data
-        console.log(data.map((p) => (p.id)));
+        console.log(data.map((p) => (p._id)));
+        const res3 = await getCategoryid("62ec840af4785a23d87da017")
+        const data3: Category[] = res3.data
+        console.log(data3.products.map((p) => (`${p.name}`)).join(""))
         return /*html*/`
             ${header.render()}
         <div class="flex mt-4 divide-x">
             ${menu.render()}
             ${banner.render()}
         </div>    
-            <h1 class="ml-20 mt-10 text-lg ">ĐIỆN THOẠI HOT NHẤT</h1> 
+        <h1 class="ml-20 mt-10 text-lg ">Sản Phẩm CHUNG</h1> 
         <div class="product ">
             
             <div class=" grid grid-cols-5 gap-6 m-10">
             ${data.map((p, index) => /*html*/`
                 <div>
-                    <a href="/products/${p.id}"><img src="${p.image}"  class="mb-2" alt=""></a>
-                    <a href="/products/${p.id}"><p class="mb-2">${p.name}</p></a>
+                    <a href="/products/${p._id}"><img src="${p.image}"  class="mb-2" alt=""></a>
+                    <a href="/products/${p._id}"><p class="mb-2">${p.name}</p></a>
                     <label class="text-red-700 pr-4">${p.saleOffPrice}</label>
                     <label class="text-gray-400">${p.originalPrice}</label>
                     <p class="bg-gray-100 p-3 mt-2">${p.shortDescription}</p>
@@ -40,42 +44,13 @@ const homePage = {
                 <p class="text-white">Phụ kiện Apple</p>
                 <img src="./public/image/phukien.jpg" alt="">
             </div>
+            ${data3.products.map((p) => (`
             <div class="bg-[#FF94EB] rounded-lg">
-                <p class="text-white">Phụ kiện Apple</p>
-                <img src="./public/image/phukien.jpg" alt="">
-                </div>
-            <div class="bg-[#96FDB5] rounded-lg">
-                <p class="text-white">Phụ kiện Apple</p>
-                <img src="./public/image/phukien.jpg" alt="">
+            <p class="text-white">${p.name}</p>
+                <img src="${p.image}" alt="">
             </div>
-            <div class="bg-[#FF94EB] rounded-lg">
-                <p class="text-white">Phụ kiện Apple</p>
-                <img src="./public/image/phukien.jpg" alt="">
-            </div>
-            <div class="bg-[#4D91FF] rounded-lg">
-                <p class="text-white">Phụ kiện Apple</p>
-                <img src="./public/image/phukien.jpg" alt="">
-            </div>
-            <div class="bg-[#F5D63D] rounded-lg">
-                <p class="text-white">Phụ kiện Apple</p>
-                <img src="./public/image/phukien.jpg" alt="">
-            </div>
-            <div class="bg-[#FDA363] rounded-lg">
-                <p class="text-white">Phụ kiện Apple</p>
-                <img src="./public/image/phukien.jpg" alt="">
-            </div>
-            <div class="bg-[#D6D6D6] rounded-lg">
-                <p class="text-white">Phụ kiện Apple</p>
-                <img src="./public/image/phukien.jpg" alt="">
-            </div>
-            <div class="bg-[#F5D63D] rounded-lg">
-                <p class="text-white">Phụ kiện Apple</p>
-                <img src="./public/image/phukien.jpg" alt="">
-            </div>
-            <div class="bg-[#FDA363] rounded-lg">
-                <p class="text-white">Phụ kiện Apple</p>
-                <img src="./public/image/phukien.jpg" alt="">
-            </div>
+            `)).join("")}
+           
         </div>
         </div>
   </div>
